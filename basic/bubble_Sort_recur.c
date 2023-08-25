4174c00ebba238f2752259735e3f6a17818930e2
Jaden Ekbote <01fe21bei043@kletech.ac.in>
2023-08-25 11:06:47 +0530

    space added

--- basic/bubble_Sort_recur.c
+++ basic/bubble_Sort_recur.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
 #include <time.h>
-#include<stdlib.h>
-void bubble(int a[],int n)
+#include <stdlib.h>
+void bubble(int a[], int n)
 {
-    if(n>0)
+    if (n > 0)
     {
-        for(int i=0;i<n;i++)
+        for (int i = 0; i < n; i++)
         {
-            bubble(a,n-1);
-            if(a[n-1]>a[n])
+            bubble(a, n - 1);
+            if (a[n - 1] > a[n])
             {
-                int temp = a[n-1];
-                a[n-1]=a[n];
-                a[n]=temp;
+                int temp = a[n - 1];
+                a[n - 1] = a[n];
+                a[n] = temp;
             }
         }
-
     }
 }
 int main()
 {
-    int a[5]={5,4,2,7,1},n;
-    bubble(a,5);
-    for(int i=0;i<5;i++)
+    int a[5] = {5, 4, 2, 7, 1}, n;
+    bubble(a, 5);
+    for (int i = 0; i < 5; i++)
     {
-        printf("%d ",a[i]);
+        printf("%d ", a[i]);
     }
-
 }
